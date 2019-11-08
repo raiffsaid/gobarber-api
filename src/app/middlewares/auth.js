1311c96eb2dac105bde961dfc6f625473cd170bf
Raiff Smith Said <raiff.said@gmail.com>
2019-11-08 18:10:23 -0300

    feat: Added an authentication middleware

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,34 @@
+// Importa a promosify que transforma uma função de callback em async/await
+import { promisify } from 'util';
+import jwt from 'jsonwebtoken';
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(400).json({
+      error: 'Token not provided'
+    });
+  }
+
+  // Separa a palavra Bearer do Token, retornando em duas posições em um array
+  // Desestruração descartando a primeira posição do array
+  const [, token] = authHeader.split(' ');
+
+  try {
+    // Transforma o jwt.verify de callback para async/await
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+    return next();
+  } catch (error) {
+    return res.status(400).json({
+      error: 'Invalid Token'
+    });
+  }
+
+  return next();
+};
--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,17 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+// Chamando o middleware globalmente
+// Apenas códigos que são chamados depois passam pelo middleware
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
