faddab66fffb164b00815d2d918a3de9df14d4fa
Raiff Smith Said <raiff.said@gmail.com>
2019-11-20 20:08:26 -0300

    feat: Added Nodemailer integration

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '054ff0228af629',
+    pass: '5fd7e4b84a96c2'
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>'
+  }
+};
